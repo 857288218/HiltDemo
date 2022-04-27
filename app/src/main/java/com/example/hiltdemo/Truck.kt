82c3bcdad4efe9fcbe5ed5a7cbe372481831da27
junqing.ren <857288218@qq.com>
2022-04-27 13:43:02 +0800

    update

--- app/src/main/java/com/example/hiltdemo/Truck.kt
+++ app/src/main/java/com/example/hiltdemo/Truck.kt
@@ -7,7 +7,7 @@ import dagger.hilt.android.scopes.ActivityRetainedScoped
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
-// 使用 @Inject 来告诉 Hilt 如何提供该类的实例，常用于构造方法、非私有字段中。
+// 使用 @Inject 来告诉 Hilt 如何提供该类的实例，常用于构造方法、类的非私有字段中。
 // Truck的构造函数中所依赖的所有其他对象都支持依赖注入了，Truck才可以被依赖注入
 class Truck @Inject constructor(private val driver: Driver, @ApplicationContext val context: Context) {
 
