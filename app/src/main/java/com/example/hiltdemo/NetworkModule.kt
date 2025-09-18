823bf2425249e98bfa0fd896105f261540a4b0eb
puppy <31814795+857288218@users.noreply.github.com>
2025-09-18 10:23:59 +0800

    补充注释

--- app/src/main/java/com/example/hiltdemo/NetworkModule.kt
+++ app/src/main/java/com/example/hiltdemo/NetworkModule.kt
@@ -17,23 +17,24 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 // 第三方类的依赖注入
-// @Module 用来提供一些无法用 构造@Inject 的依赖，如第三方库，接口，build 模式的构造等。
+// @Module 用来提供一些无法用 构造@Inject 的依赖，如第三方库、接口、build模式的构造等。
 // 使用 @Module 注解的类，需要使用 @InstallIn 注解指定 module 的范围
 // 增加了 @Module 注解的类，其实代表的就是一个模块，并通过指定的组件来告诉在哪些组件中可以使用绑定安装。
 
-// @InstallIn注解指定 module 的范围,在该组件范围内(包括以该组件为父组件的组件)可以使用由这个module提供的所有依赖注入实例
-// 例如@InstallIn(ActivityComponent::class)，就是把这个模块安装到Activity组件当中。
-// 安装到Activity组件中，那么在Activity中是可以使用由这个module提供的所有依赖注入实例,Activity中包含的Fragment和View也可以使用，但是除Activity、Fragment、View之外的其他地方就无法使用了。
-// 比如说，我们在Service/ViewModel中使用@Inject来对Retrofit/OkHttpClient类型的字段进行依赖注入，就一定会报错。
-// 这里使用的SingletonComponent为全局组件，依赖注入实例可以在全项目中使用
+// @InstallIn注解指定 module 的范围,在该组件范围内(包括以该组件为父组件的组件)可以使用由这个module提供的所有依赖注入实例。
+// 例如@InstallIn(ActivityComponent::class)，就是把这个模块安装到Activity组件中,会为每一个Activity实例创建一个对应的ActivityComponent，
+// ActivityA实例有自己的ActivityComponent，ActivityB实例也有自己的ActivityComponent，它们是两个不同的组件实例，所以A和B中拿到的OkHttpClient也是不同实例，不同Activity实例拿到的是不同OkHttpClient实例。
+// 在Activity中是可以使用由这个module提供的所有依赖注入实例,Activity实例中包含的Fragment和View也可以使用(且拿到的和所属Activity实例中拿到的是同一个实例)，
+// 但是除Activity、Fragment、View之外的其他地方就无法使用了，在Service/ViewModel中使用@Inject来对Retrofit/OkHttpClient类型的字段进行依赖注入，就一定会报错。
+// 这里使用的 SingletonComponent 为全局组件，依赖注入实例可以在全项目中使用。
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
     // @Provides常用于被 @Module 注解标记类的内部方法上。并提供依赖项对象。
     @Provides
-    // Module中方法上添加的作用域注解(@Singleton,@ActivityScoped等)需要和@InstallIn组件对应否则编译不过，或不添加作用域注解 每次对OkHttpClient依赖注入都会创建新的实例
-    // 例如@InstallIn(ActivityComponent::class)那么在方法上添加作用域注解只能添加@ActivityScoped
+    // Module中方法上添加的作用域注解(@Singleton,@ActivityScoped等)需要和@InstallIn组件对应否则编译不过，或不添加作用域注解 每次对OkHttpClient依赖注入都会创建新实例
+    // 例如@InstallIn(ActivityComponent::class)那么在方法上添加作用域注解只能添加@ActivityScoped,这个注解的含义是在同一个ActivityComponent的生命周期内，被它注解的依赖项只会创建一个实例。
     @Singleton
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
@@ -44,7 +45,7 @@ class NetworkModule {
     }
 
     //provideRetrofit()函数接收了一个OkHttpClient参数，并且在创建Retrofit实例的时候还依赖了这个参数。那么你可能会问了，我们要如何向provideRetrofit()函数去传递OkHttpClient这个参数呢？
-    //答案是，完全不需要传递，因为这个过程是由Hilt自动完成的。我们所需要做的就是保证Hilt能知道如何得到一个OkHttpClient的实例，而这个工作在provideOkHttpClient()中已经完成了。
+    //完全不需要传递，因为这个过程是由Hilt自动完成的。我们所需要做的就是保证Hilt能知道如何得到一个OkHttpClient的实例，而这个工作在provideOkHttpClient()中已经完成了。
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
